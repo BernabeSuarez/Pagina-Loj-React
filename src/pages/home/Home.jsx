90fd028e175ba89acdefaa1476ba022df0b0d722
Bernabe <bernatattoo@gmail.com>
2022-07-25 12:18:06 -0300

    fixed media queries

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,15 +6,31 @@ const Container = styled.div`
   height: 100vh;
   padding: 4rem 12rem 4rem;
   color: #ebebeb;
+  @media (max-width: 576px) {
+    padding: 2%;
+  }
 `;
 const Title = styled.h1`
   text-align: center;
-  left: 12rem;
+  @media (max-width: 576px) {
+    padding: 2%;
+    font-size: 1.5rem;
+  }
+`;
+const H2 = styled.h2`
+  text-align: center;
+  font-family: monospace;
+  font-weight: 100;
+  @media (max-width: 576px) {
+    padding: 2%;
+    font-size: 1.2rem;
+  }
 `;
 const Home = () => {
   return (
     <Container>
       <Title>Bienvenidos a la pagina de La Otra Junta</Title>
+      <H2>Pagina en construccion</H2>
     </Container>
   );
 };
