f38ec7971516d71135a4f8f607e663641d1022a2
Bernabe <bernatattoo@gmail.com>
2022-07-26 22:43:25 -0300

    Correccion de texto

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -25,28 +25,29 @@ const About = () => {
       <Title>La Otra junta</Title>
       <TextContainer>
         <P>
-          Somos un grupo folclorico formado a finales del 2015, influenciado por
-          la nueva camada de la musica popular argentina, provinientes de
-          diferentes estilos musicales, buscando una fusion de estos estilos con
+          Somos un grupo folclórico formado a finales del 2015, influenciado por
+          la nueva camada de la música popular argentina, provenientes de
+          diferentes estilos musicales, buscando una fusión de estos estilos con
           el folclore como base.
         </P>
+
         <P>
-          Hace varios años ya que venimos presentandonos en diferentes
-          escenarios del interior de la provincia de Buenos Aires: Ascencion,
-          Salto, Junin, Lujan, Chivilcoy, Bragado, Pergamino, Gral. Villegas,
-          Capital Federal, La Plata, Etc. y en la mayoria de las peñas del
-          noroeste bonaerense.
+          Hace varios años que venimos presentándonos en diferentes escenarios
+          del interior de la provincia de Buenos Aires: Ascensión, Salto, Junín,
+          Luján, Chivilcoy, Bragado, Pergamino, Gral. Villegas, Capital Federal,
+          La Plata, Etc. y en la mayoría de las peñas del noroeste bonaerense.
+        </P>
+
+        <P>
+          En 2019 participamos del los espectáculos callejeros del Festival de
+          Cosquín, además de tocar en las peñas más importantes; La fiesta del
+          violinero, La Salamanca, el fogón Criollo oficial de Cosquín, entre
+          otras más.
         </P>
         <P>
-          En 2019 participamos del los espectaculos callejeros del Festival de
-          Cosquin ademas de tocar en las peñas mas importantes; La fiesta del
-          violinero, La Salamanca, el fogon Criollo oficial de Cosquin, entre
-          otras mas.
-        </P>
-        <P>
-          Actualmente La Otra Junta esta conformada por: Nicolas Cavaglia
-          (Guitarra y Voz) Paulo Mocse (Bajo y Voz), Nestor Toscanini (Bateria),
-          Facundo Sorba (Guitarra), Bernabe Suarez( Guitarra y Violin)
+          Actualmente, La Otra Junta está conformada por: Nicolás Cavaglia
+          (Guitarra y Voz) Paulo Mocse (Bajo y Voz), Néstor Toscanini (Bateria),
+          Facundo Sorba (Guitarra), Bernabé Suarez( Guitarra y Violín).
         </P>
       </TextContainer>
     </Container>
