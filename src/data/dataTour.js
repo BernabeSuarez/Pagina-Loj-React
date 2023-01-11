3185ebebf291c5ae6d1fe690840a770df4b7576b
Bernabe <bernatattoo@gmail.com>
2023-01-10 23:11:19 -0300

    No dataTour

--- src/data/dataTour.js
+++ src/data/dataTour.js
@@ -1,20 +1,14 @@
 export const dataTour = [
 
-
-    {
-        date: "Jueves 5 de Enero",
-        name: "Me Matan Limon",
-        time: "21:00 Hs.",
-        place: "Leandro N. Alem - Buenos Aires",
-        url: "https://goo.gl/maps/NzoNR6SnWx2qjYECA"
-    },
-    {
-        date: "Sabado 7 de Enero",
-        name: "Aniversario de El Dorado",
-        time: "21:00 Hs.",
-        place: "El Dorado - Buenos Aires",
-        url: "https://goo.gl/maps/zgmNkTBxqB1qfYX69"
-    },
+    /*
+        {
+            date: "Jueves 5 de Enero",
+            name: "Me Matan Limon",
+            time: "21:00 Hs.",
+            place: "Leandro N. Alem - Buenos Aires",
+            url: "https://goo.gl/maps/NzoNR6SnWx2qjYECA"
+        },
+     */
 
 ]
 
