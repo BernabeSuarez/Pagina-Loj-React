3ef7fb34da78eeb42d12bd5efac05e83fbdbb60f
Bernabe <bernatattoo@gmail.com>
2023-02-03 13:22:34 -0300

    fix image extension

--- src/data/dataGallery.js
+++ src/data/dataGallery.js
@@ -1,55 +1,55 @@
 export const dataGallery = [
     {
-        img: "img/1.webp",
+        img: "img/1.jpg",
         rows: 2,
         title: "La Otra Junta - Facu"
     },
     {
-        img: "img/2.webp",
+        img: "img/2.jpg",
         rows: 2,
         cols: 1,
         title: "La Otra Junta - Nico"
     },
     {
-        img: "img/3.webp",
+        img: "img/3.jpg",
         rows: 3,
         title: "La Otra Junta - Paulo"
     },
     {
-        img: "img/4.webp",
+        img: "img/4.jpg",
         rows: 2,
         cols: 2,
         title: "La Otra Junta - Toska"
     },
     {
-        img: "img/5.webp",
+        img: "img/5.jpg",
         rows: 2,
         title: "La Otra Junta - Facu"
     },
     {
-        img: "img/6.webp",
+        img: "img/6.jpg",
         rows: 2,
         title: "La Otra Junta - Paulo"
 
     },
     {
-        img: "img/7.webp",
+        img: "img/7.jpg",
         rows: 1,
         title: "La Otra Junta - Berna"
     },
     {
-        img: "img/8.webp",
+        img: "img/8.jpg",
         rows: 2,
         title: "La Otra Junta - Banda"
     },
     {
-        img: "img/9.webp",
+        img: "img/9.jpg",
         rows: 2,
         cols: 1,
         title: "La Otra Junta - Banda"
     },
     {
-        img: "img/10.webp",
+        img: "img/10.jpg",
         rows: 1,
         title: "La Otra Junta - Banda"
     }
