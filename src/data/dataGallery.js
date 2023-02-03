528b305744c2f0c1950f280e65c6f3e854419454
Bernabe <bernatattoo@gmail.com>
2023-02-03 13:17:52 -0300

    add Flyer to News section

--- src/data/dataGallery.js
+++ src/data/dataGallery.js
@@ -1,55 +1,55 @@
 export const dataGallery = [
     {
-        img: "img/1.jpg",
+        img: "img/1.webp",
         rows: 2,
         title: "La Otra Junta - Facu"
     },
     {
-        img: "img/2.jpg",
+        img: "img/2.webp",
         rows: 2,
         cols: 1,
         title: "La Otra Junta - Nico"
     },
     {
-        img: "img/3.jpg",
+        img: "img/3.webp",
         rows: 3,
         title: "La Otra Junta - Paulo"
     },
     {
-        img: "img/4.jpg",
+        img: "img/4.webp",
         rows: 2,
         cols: 2,
         title: "La Otra Junta - Toska"
     },
     {
-        img: "img/5.jpg",
+        img: "img/5.webp",
         rows: 2,
         title: "La Otra Junta - Facu"
     },
     {
-        img: "img/6.jpg",
+        img: "img/6.webp",
         rows: 2,
         title: "La Otra Junta - Paulo"
 
     },
     {
-        img: "img/7.jpg",
+        img: "img/7.webp",
         rows: 1,
         title: "La Otra Junta - Berna"
     },
     {
-        img: "img/8.jpg",
+        img: "img/8.webp",
         rows: 2,
         title: "La Otra Junta - Banda"
     },
     {
-        img: "img/9.jpg",
+        img: "img/9.webp",
         rows: 2,
         cols: 1,
         title: "La Otra Junta - Banda"
     },
     {
-        img: "img/10.jpg",
+        img: "img/10.webp",
         rows: 1,
         title: "La Otra Junta - Banda"
     }
