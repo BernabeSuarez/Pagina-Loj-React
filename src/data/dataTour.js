1df3ac779e0fb91b4368368b5ffb12f8ec8311b0
Bernabe <bernatattoo@gmail.com>
2023-03-05 11:24:20 -0300

    mod fechas tour

--- src/data/dataTour.js
+++ src/data/dataTour.js
@@ -1,13 +1,13 @@
 export const dataTour = [
-    /* {
-         "date": "Sabado 4 febrero",
-         "name": "Flow Almacen de Pizzas",
-         "time": "21:00 Hs.",
-         "place": "Vedia - Buenos Aires",
-         "url": "https://goo.gl/maps/NzoNR6SnWx2qjYECA",
-         "src": "img/flow.webp"
-     }
-     */
+    {
+        "date": "Sabado 11 de Marzo",
+        "name": "Club Union El Dorado",
+        "time": "21:00 Hs.",
+        "place": "El dorado",
+        "url": "https://goo.gl/maps/c7ZmUEBR6tg1LTiU8",
+        "src": "img/festival.webp"
+    }
+
 ]
 
 //Plantilla datos 
